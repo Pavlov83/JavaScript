30372450f9fcd43c1baa56cb1e4f1a3a1ed5a0f8
Pavlov83 <codedriverun@gmail.com>
2018-07-01 09:14:24 +0300

    examining the dom

---
+++ Complete_Javascript_from_the_beginning/examining_the_DOM/app.js
@@ -0,0 +1,12 @@
+let val;
+
+val = document;
+val = document.all[1];// gives us the second item of the DOM ( the head tag)
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.URL;
+val = document.contentType;
+
+
+console.log(val);
