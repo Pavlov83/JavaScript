9b43d55235114eecc13523061c5a70480c54db06
Pavlov83 <codedriverun@gmail.com>
2018-07-05 20:44:29 +0300

    event listeners

---
+++ Complete_Javascript_from_the_beginning/Event_listeners/app.js
@@ -0,0 +1,38 @@
+document.querySelector('.clear-tasks').addEventListener('click',
+function(e){
+    console.log('Hello World');
+    //e.preventDefault();
+});
+
+document.querySelector('.clear-tasks').addEventListener('click',onClick);
+
+function onClick(e){
+    //console.log('clicked');
+
+    let val;
+
+    //Event target element
+    val = e.target;
+    val = e.target.id;
+    val = e.className;
+    val = e.target.classList;
+
+    e.target.innerText = 'Hello';
+
+    //Event type
+    val = e.type;
+
+    //Timestamp
+
+    val = e.timeStamp;
+
+    //Coords event relative to the window
+    val = e.clientY;
+    val = e.clientX;
+
+    //Coords event relative to the element
+    val = e.offsetY;
+    val = e.clientX;
+
+    console.log(val);
+}
